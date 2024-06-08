ebb172cbf21393fda02ae4c0cf23e34389ee5e1c
Dpiashaft <dpiashaft.matei@gmail.com>
2024-06-08 17:35:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include <Windows.h>
 
-
 using namespace std;
-
 
 int main()
 {
 	int newVal = 456;
 	int readVal = 0;
-
 	HWND hwnd = FindWindowA(NULL, "Test");
-
 	if (hwnd == NULL)
 	{
-
 		MessageBoxA(0, "Cannot find window!", "Cannot find window!", MB_ICONERROR);
 		Sleep(3000);
 		exit(-1);
-
 	}
 	else
 	{
@@ -32,23 +26,14 @@ int main()
 			MessageBoxA(0, "Cannot find process!", "Cannot find process!", MB_ICONERROR);
 			Sleep(3000);
 			exit(-1);
-
-
 		}
 		else
 		{
 			ReadProcessMemory(hProcess, (PBYTE*)0x6fff0c, &readVal, sizeof(readVal), 0);
 			cout << "The standard value of i:" << readVal << endl;
-
 			WriteProcessMemory(hProcess, (PBYTE*)0x6fff0c, &newVal, sizeof(newVal), 0);
-
 			cout << "The new value of i:" << newVal << endl;
-
 			Sleep(6000);
 		}
-
 	}
-
-
-
 }
